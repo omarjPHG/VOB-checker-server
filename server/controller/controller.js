5b5343dfde9b5add2ffe17af50f19f42d9a4ad63
Omar Jandali <omarj@premierhealthgroup.net>
2023-12-05 15:24:17 -0800

    Omar Jandali: Adjusted loading endpoint to determine the evaluation of each record based on the request that is sent.

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,31 +1,16 @@
 const { db } = require('../bin/utils/Firebase')
 const { addDoc, collection, query, where, onSnapshot } = require('firebase/firestore')
+const axios = require('axios')
+const { exec } = require('child_process');
 
-const addCustomer = (customerName, insurances, res) => {
-    const collectionCustomerRef = collection(db, 'Customers')
-    addDoc(collectionCustomerRef, {
-        'customerName':customerName,
-        'insurancePrefix': insurances[0].insurancePrefix,
-        'insuranceLoc': insurances[0].insuranceLoc,
-        'dailyRate': insurances[0].dailyRate,
-        'evaluation': insurances[0].evaluation,
-        'lastUpdate': insurances[0].lastUpdate,
-        'callInDate': insurances[0].callInDate,
-    })
-    .then(response => {
-        res.send('User Information Was Inputted Into Database').status(200)
-    })
-    .catch(error => {
-        console.error(error)
-        res.send(`Error: ${error}`).status(400)
-    })
-}
+//  ssh -i ./digitalOcean root@24.144.87.113
 
 const garyController = {
     post: (req, res) => {
-        const customerName = req.body.customerName
+        let docId
         const insuranceLoc = req.body.insuranceLoc 
         const insurancePrefix = req.body.insurancePrefix 
+        const question = `based on provided information, is '${insurancePrefix}' considered to be VOB good? if there are different instances, decide based on most instances. explain your reasoning step-by-step`;
         const collectionRef = collection(db, 'CurrentInsurance')
         let q = query(collectionRef, 
             where('insuranceLoc', '==', insuranceLoc), 
@@ -33,54 +18,87 @@ const garyController = {
         onSnapshot(q, snapshot => {
             const insurances = []
             snapshot.docs.forEach(doc => {
-                console.log(doc.data())
                 insurances.push(doc.data())
+                docId = doc.id
             })
-            insurances.length === 1
-                ? addCustomer(customerName, insurances, res)
-                : res.send(`Error: grabbing the insurance`).status(400)
         })
-    }
+        runAssistant(question)
+        res.send("completed").status(200)
+    },
 }
 
 const interfaceController = {
     get: (req, res) => {
-        const collectionRef = collection(db, 'Customers')
+        const collectionRef = collection(db, 'CurrentInsurance')
         const q = query(collectionRef)
         onSnapshot(q, snapshot => {
             let customerList = []
             snapshot.docs.forEach(doc => {
-                customerList.push(doc.data())
+                customerList.push({data: doc.data(), id: doc.id})
             })
-            console.log(customerList)
             res.send(customerList).status(200)
         })
+    },
+    update: (req, res) => {
+
     }
 }
 
 const dbLoading = {
     post: (req, res) => {
+        let evaluationType
+        let insuranceName = req.body.insuranceName
         let insuranceLoc = req.body.insuranceLoc
-        let prefix = req.body.prefix 
+        let insurancePrefix = req.body.insurancePrefix 
         let dailyRate = req.body.dailyRate 
         let lastUpdate = req.body.lastUpdate
-        let evaluation = req.body.evaluation
-        let collectionRef = collection(db, 'CurrentInsurance')
-        addDoc(collectionRef, {
-            'insuranceLoc': insuranceLoc,
-            'insurancePrefix': prefix,
-            'dailyRate': dailyRate,
-            'lastUpdate': lastUpdate,
-            'evaluation': evaluation,
-            'callInDate': new Date()
-        })
-        .then(response => {
-            res.send(`Interface Added ${insuranceLoc} - ${prefix} - ${dailyRate} UPDATED: ${lastUpdate}`).status(200)
-        })
-        .catch(error => {
-            res.send(`Error: ${error}`).status(400)
-        })
+        runPythonScript(insurancePrefix)
+            .then(response => {
+                console.log(`python script response: ${response}`)
+                response.includes("Yes")
+                    ? evaluationType = 'Yes'
+                    : response.includes("No")
+                        ? evaluationType = 'Yes'
+                        : evaluationType = 'Unknown'
+                let collectionRef = collection(db, 'CurrentInsurance')
+                addDoc(collectionRef, {
+                    'insuranceName': insuranceName,
+                    'insuranceLoc': insuranceLoc,
+                    'insurancePrefix': insurancePrefix,
+                    'dailyRate': dailyRate,
+                    'lastUpdate': lastUpdate,
+                    'evaluation': evaluationType,
+                    'callInDate': new Date()
+                })
+                .then(response => {
+                    res.send(`Interface Added ${insuranceLoc} - ${insurancePrefix} - ${evaluationType}`).status(200)
+                })
+                .catch(error => {
+                    res.send(`Error: ${error}`).status(400)
+                })
+            })
+            .catch(error => {
+                console.error(`there was an error getting a response: `, error)
+            })
     }
 }
+
+const runPythonScript = (prefix) => {
+    return new Promise((resolve, reject) => {
+        exec(`python ./server/controller/evaluation.py ${prefix}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                reject(error);
+                return;
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                reject(stderr);
+                return;
+            }
+            resolve(stdout);
+        });
+    });
+};
 
 module.exports = {garyController, dbLoading, interfaceController}
---
+++ server/controller/evaluation.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+import openai
+from dotenv import load_dotenv
+import time
+from config import OPENAI_API_KEY
+
+def run_assistant(question):
+    openai.api_key = OPENAI_API_KEY
+    try:
+        run = openai.beta.threads.create_and_run(
+            assistant_id="asst_uHIcUFcG983U24n1ohEvXk42",
+            thread={
+                "messages": [
+                    {"role": "user", "content": question}
+                ]
+            }
+        )
+        return run
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def get_answer(run):
+    # Access attributes directly from the 'run' object
+    thread_id = run.thread_id
+    run_id = run.id
+
+    while run.status != "completed":
+        time.sleep(1)  # Adding a delay to avoid too frequent polling
+        try:
+            run = openai.beta.threads.runs.retrieve(
+                thread_id=thread_id,
+                run_id=run_id
+            )
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None, None
+
+    try:
+        messages = openai.beta.threads.messages.list(
+            thread_id=thread_id
+        )
+
+        # Adjusting to the correct structure of messages
+        annotations = messages.data[0].content[0].text.annotations
+        message_content = messages.data[0].content[0].text.value
+        return message_content
+    except Exception as e:
+        print(f"An error occurred while retrieving messages: {e}")
+        return None, None
+
+
+arg1 = sys.argv[1]
+question = f"based on provided file, with the prefix of '{arg1}' in the prefix column, is '{arg1}' considered to be a good VOB based ont he Good Vob column? Answer the question with a Yes or No. If the record is not found answer as Unknown."
+    
+chat = run_assistant(question)
+response = get_answer(chat)
+print(response)
